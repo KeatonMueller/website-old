b9e7bf3e15799a7a5ace602888670e27b9d587fe
Keaton Mueller <keaton@Keatons-MacBook-Pro.local>
2019-08-04 19:02:05 -0400

    Updates

--- precache-manifest.c913e4066fa2b6d5b59fb51ab00c02ca.js
+++ precache-manifest.c913e4066fa2b6d5b59fb51ab00c02ca.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "eca9badf3b9e824de04a30c76f9bab1f",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
+    "revision": "6346f212cad8157f0356",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "2e0129063922e3bb197a",
-    "url": "/static/js/2.b41502e9.chunk.js"
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "6346f212cad8157f0356",
+    "url": "/static/js/main.fda272dd.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.c913e4066fa2b6d5b59fb51ab00c02ca.js"
 );
 
 self.addEventListener('message', (event) => {
