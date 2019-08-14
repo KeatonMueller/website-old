3c2981006e6af2e4e9a22b8cab80c569681f6e2b
Keaton Mueller <keaton@Keatons-MacBook-Pro.local>
2019-08-13 23:17:37 -0400

    Updates

---
+++ precache-manifest.afee87ffec7785d601749e45524bcb2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a654dd8cb379ebc0580a4a5a4677ee31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c22e996869d6b896f733",
+    "url": "/static/js/2.32c63a89.chunk.js"
+  },
+  {
+    "revision": "a79a49473a68c62f7bc4",
+    "url": "/static/js/main.1f125953.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c67c40b7862ee4260be4e35a56ace68",
+    "url": "/static/media/Portrait.8c67c40b.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07249ae1f67ceeec60e4524cc4861e68.js"
+  "/precache-manifest.afee87ffec7785d601749e45524bcb2b.js"
 );
 
 self.addEventListener('message', (event) => {
