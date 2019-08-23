96a87d03b2adbe2c850dab07d869922f51fa2cd9
Keaton Mueller <keaton.mueller@yale.edu>
2019-08-22 20:50:10 -0400

    Updated gradient

--- src/Styles.js
+++ src/Styles.js
@@ -65,7 +65,7 @@ export default (theme) => (
 			}
 		},
 		titleBar: {
-			background: 'linear-gradient(to bottom, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.5) 70%, rgba(0,0,0,0) 100%)'
+			background: 'linear-gradient(to bottom, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.5) 50%, rgba(0,0,0,0) 100%)'
 		},
 		bold: {
 			fontWeight: 'bold'
